d4cc40a37e415f57703f32fa3e7934e2d241f4a8
MD Rubayet Hassan <rubayet.evan@gmail.com>
2019-09-05 12:58:01 +0600

    bug fixed

--- lib/src/utilities/commonWidgets.dart
+++ lib/src/utilities/commonWidgets.dart
@@ -43,38 +43,39 @@ class Commons {
             Container(
               height: 40.0,
               width: 100.0,
-              padding: EdgeInsets.all(10.0),
               decoration: BoxDecoration(
                   border: Border.all(color: Constants.primaryColor),
                   borderRadius: BorderRadius.all(Radius.circular(8.0))),
-              child: StreamBuilder(
-                  stream: stream,
-                  initialData: init,
-                  builder: (context, snapshot) {
-                    if (snapshot.data.toString() != _controller.text &&
-                        snapshot.data.toString() != null &&
-                        snapshot.data is String) {
-                      _controller.text = snapshot.data.toString();
-                      _controller.selection = TextSelection.collapsed(
-                          offset: _controller.text.length);
-                    }
-                    return TextField(
-                      controller: _controller,
-                      inputFormatters: [
-                        LengthLimitingTextInputFormatter(4),
-                        WhitelistingTextInputFormatter.digitsOnly,
-                      ],
-                      textAlign: TextAlign.center,
-                      decoration: InputDecoration.collapsed(hintText: ''),
-                      cursorColor: Constants.primaryColor,
-                      keyboardType: TextInputType.number,
-                      onChanged: (value) {
-                        changeFunction(value);
-                        _controller.selection =
-                            TextSelection.collapsed(offset: value.length);
-                      },
-                    );
-                  }),
+              child: Center(
+                child: StreamBuilder(
+                    stream: stream,
+                    initialData: init,
+                    builder: (context, snapshot) {
+                      if (snapshot.data.toString() != _controller.text &&
+                          snapshot.data.toString() != null &&
+                          snapshot.data is String) {
+                        _controller.text = snapshot.data.toString();
+                        _controller.selection = TextSelection.collapsed(
+                            offset: _controller.text.length);
+                      }
+                      return TextField(
+                        controller: _controller,
+                        inputFormatters: [
+                          LengthLimitingTextInputFormatter(4),
+                          WhitelistingTextInputFormatter.digitsOnly,
+                        ],
+                        textAlign: TextAlign.center,
+                        decoration: InputDecoration.collapsed(hintText: ''),
+                        cursorColor: Constants.primaryColor,
+                        keyboardType: TextInputType.number,
+                        onChanged: (value) {
+                          changeFunction(value);
+                          _controller.selection =
+                              TextSelection.collapsed(offset: value.length);
+                        },
+                      );
+                    }),
+              ),
             ),
             IconButton(
                 icon: new Icon(Icons.add_circle_outline),
